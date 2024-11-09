276446490ab0b15c6795d90eaefc4d95070dab1a
wccalvin <clayton@wccalvin.com>
2024-11-09 06:10:26 -0500

    example of enum

--- basics.cpp
+++ basics.cpp
@@ -6,6 +6,7 @@ int main() {
   // first program (an example of single line comments)
   cout << "--------------> 1 <--------------" << endl;
   cout << "hello, world!" << endl;
+  cout << endl;
 
   /* example of
    * multi-line
@@ -22,10 +23,29 @@ int main() {
   cout << "float: " << sizeof(float) << " bytes" << endl;
   cout << "double: " << sizeof(double) << " bytes" << endl;
   cout << "bool: " << sizeof(bool) << " bytes" << endl;
+  cout << endl;
 
-  cout << "--------------> 2 <--------------" << endl;
-  // variable difined constant cannot be changed
+  cout << "--------------> 3 <--------------" << endl;
+  // const variable cannot be changed
   const std::string name = "clayton";
   cout << name << endl;
+  cout << endl;
+
+  cout << "--------------> 4 <--------------" << endl;
+  // enums
+  enum COLORS { Red, Orange, Yellow, Green, Blue, Indigo, Violet };
+  COLORS favColor = Blue;
+  cout << favColor << endl;  // prints the index #4
+  // however we can compare the values but can't print values like you would
+  // in an array or vector
+  if (favColor == Yellow) {
+    cout << "Halo!\n";
+  } else if (favColor == Blue) {
+    cout << "Blue\n";
+  } else {
+    cout << "Some other color\n";
+  }
+  cout << endl;
+
   return 0;
 }
